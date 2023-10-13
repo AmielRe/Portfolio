1c44586df2ac4cdcc8b63723ba95d6bd618fc3d7
AmielRe <amieli1999@gmail.com>
2023-10-13 13:17:10 +0300

    Add assets and constants.

---
+++ src/constants.js
@@ -0,0 +1,116 @@
+export const techStack = [
+  {
+    name: "CSharp",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-original.svg",
+  },
+
+  {
+    name: "Java",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg",
+  },
+  {
+    name: "Docker",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg",
+  },
+
+  {
+    name: "Git",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
+  },
+  {
+    name: "Hibernate",
+    link: "https://www.vectorlogo.zone/logos/hibernate/hibernate-icon.svg",
+  },
+  {
+    name: "Spring",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/spring/spring-original.svg",
+  },
+
+  {
+    name: "Python",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg",
+  },
+  {
+    name: "RabbitMQ",
+    link: "https://www.vectorlogo.zone/logos/rabbitmq/rabbitmq-icon.svg",
+  },
+  {
+    name: "Openshift",
+    link: "https://www.vectorlogo.zone/logos/openshift/openshift-icon.svg",
+  },
+  {
+    name: "HTML",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
+  },
+  {
+    name: "CSS",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
+  },
+  {
+    name: "Bootstrap",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg",
+  },
+  {
+    name: "PostgreSQL",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg",
+  },
+
+  {
+    name: "Javascript",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+  },
+  {
+    name: "React",
+    link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg",
+  },
+  {
+    name: "Android Studio",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/androidstudio/androidstudio-original.svg",
+  },
+  {
+    name: "Mongo DB",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg",
+  },
+  {
+    name: "Jenkins",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jenkins/jenkins-original.svg",
+  },
+  {
+    name: "Firebase",
+    link: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+  },
+  {
+    name: "VS Code",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg",
+  },
+];
+
+export const contactLinks = [
+  {
+    name: "Gmail",
+    url: "https://img.icons8.com/doodle/40/000000/gmail-new--v2.png",
+    link: "mailto:amieli1999@gmail.com",
+  },
+  {
+    name: "Github",
+    url: "https://img.icons8.com/doodle/40/000000/github--v1.png",
+    link: "https://github.com/AmielRe",
+  },
+  {
+    name: "LinkedIn",
+    url: "https://img.icons8.com/doodle/40/000000/linkedin--v2.png",
+    link: "https://www.linkedin.com/in/amiel-refaeli/",
+  },
+];
+
+export const serviceData = [
+  {
+    name: "Backend Development",
+    desc: `My abilities as a backend developer encompass expertise in scaling,
+    microservices, and crafting intricate, robust solutions. I excel in designing
+    scalable systems using microservices architecture, which enhances flexibility
+    and performance. My focus on solid architecture ensures reliability, making me
+    adept at handling complex challenges effectively.`,
+    img: "https://img.icons8.com/external-flaticons-flat-flat-icons/64/000000/external-backend-no-code-flaticons-flat-flat-icons.png",
+  },
+];
