8318211caa5340e75e92fb7809f1d93071f45d95
AmielRe <amieli1999@gmail.com>
2023-10-14 21:09:42 +0300

    Enable resume download.

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -67,12 +67,16 @@ const Home = () => {
             </div>
             <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
               <div className="mt-3 sm:mt-0 cursor-pointer w-1/2">
-                <Link
+                <a
                   style={{ width: 250 }}
                   className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-500 hover:bg-blue-200 md:py-4 md:text-lg md:px-10"
+                  href="./Amiel_Refaeli.pdf"
+                  download="Amiel_Refaeli.pdf"
+                  target="_blank"
+                  rel="noreferrer"
                 >
                   Resume
-                </Link>
+                </a>
               </div>
             </div>
           </div>
